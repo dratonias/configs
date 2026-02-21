19927dfa6e35786d6bea195f58856853cd990250
Dratonias <dratonias@gmail.com>
2026-02-21 12:25:57 +0100

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
---vim.cmd("language en_US")
+vim.cmd("language en_US")
 
 vim.opt.tabstop = 1
 vim.opt.softtabstop = 1
@@ -183,7 +183,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
- "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+ -- "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
  -- NOTE: Plugins can also be added by using a table,
  -- with the first argument being the link and the following
